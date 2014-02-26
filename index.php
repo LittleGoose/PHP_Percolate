9d1654b03def87f138e6f7620bc181968854dcab
LittleGoose <jesse27999@gmail.com>
2014-02-26 16:28:32 -0500

    Made URLs correct and added more links

--- index.php
+++ index.php
@@ -16,68 +16,85 @@
         <div id="main">
             <h4>Chapter 1</h4>
 
-            <a href="http://jesseevers.zxq.net/chapter_1/template.html">template.html - 1.1</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/first.php">first.php - 1.2</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/second.php">second.php - 1.3</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/comments.php">comments.php - 1.4</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/predefined.php">predefined.php - 1.5</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/strings.php">strings.php - 1.6</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/concat.php">concat.php - 1.7</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/numbers.php">numbers.php - 1.8</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/constants.php">constants.php - 1.9</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/quotes.php">quotes.php - 1.10</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/review.php">review.php - Review</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_1/pursue.php">pursue.php - Pursue</a>
+            <a href="http://jesseevers.com/chapter_1/template.html">template.html - 1.1</a><br />
+            <a href="http://jesseevers.com/chapter_1/first.php">first.php - 1.2</a><br />
+            <a href="http://jesseevers.com/chapter_1/second.php">second.php - 1.3</a><br />
+            <a href="http://jesseevers.com/chapter_1/comments.php">comments.php - 1.4</a><br />
+            <a href="http://jesseevers.com/chapter_1/predefined.php">predefined.php - 1.5</a><br />
+            <a href="http://jesseevers.com/chapter_1/strings.php">strings.php - 1.6</a><br />
+            <a href="http://jesseevers.com/chapter_1/concat.php">concat.php - 1.7</a><br />
+            <a href="http://jesseevers.com/chapter_1/numbers.php">numbers.php - 1.8</a><br />
+            <a href="http://jesseevers.com/chapter_1/constants.php">constants.php - 1.9</a><br />
+            <a href="http://jesseevers.com/chapter_1/quotes.php">quotes.php - 1.10</a><br />
+            <a href="http://jesseevers.com/chapter_1/review.php">review.php - Review</a><br />
+            <a href="http://jesseevers.com/chapter_1/pursue.php">pursue.php - Pursue</a>
                           
             <h4>Chapter 2</h4>
 
-            <a href="http://jesseevers.zxq.net/chapter_2/form.html">form.html - 2.1</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/form2.html">form2.html - This is the same as form.html but using handle_form2.php</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/form3.html">form3.html - This is the same as form.html but using handle_form3.php</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/form4.html">form4.html - This is the same as form.html but using handle_form4.php</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/calendar.php">calendar.php - 2.6</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/multi.php">multi.php - 2.7</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/sorting.php">sorting.php - 2.8</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/calendar2.php">calendar2.php - 2.9</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_2/review.php">review.php - Review</a><br />
+            <a href="http://jesseevers.com/chapter_2/form.html">form.html - 2.1</a><br />
+            <a href="http://jesseevers.com/chapter_2/form2.html">form2.html - This is the same as form.html but using handle_form2.php</a><br />
+            <a href="http://jesseevers.com/chapter_2/form3.html">form3.html - This is the same as form.html but using handle_form3.php</a><br />
+            <a href="http://jesseevers.com/chapter_2/form4.html">form4.html - This is the same as form.html but using handle_form4.php</a><br />
+            <a href="http://jesseevers.com/chapter_2/calendar.php">calendar.php - 2.6</a><br />
+            <a href="http://jesseevers.com/chapter_2/multi.php">multi.php - 2.7</a><br />
+            <a href="http://jesseevers.com/chapter_2/sorting.php">sorting.php - 2.8</a><br />
+            <a href="http://jesseevers.com/chapter_2/calendar2.php">calendar2.php - 2.9</a><br />
+            <a href="http://jesseevers.com/chapter_2/review.php">review.php - Review</a><br />
 
             <h4>Chapter 3</h4>
 
-            <a href="http://jesseevers.zxq.net/chapter_3/index.php">index.php - 3.4</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/calculator.php">calculator.php - 3.5</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/calculator2.php">calculator2.php - 3.6</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/index2.php">index2.php - 3.7</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/calculator3.php">calculator3.php - 3.8</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/calculator4.php">calculator4.php - 3.9</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_3/calculator5.php">calculator5.php - 3.10</a><br />
+            <a href="http://jesseevers.com/chapter_3/index.php">index.php - 3.4</a><br />
+            <a href="http://jesseevers.com/chapter_3/calculator.php">calculator.php - 3.5</a><br />
+            <a href="http://jesseevers.com/chapter_3/calculator2.php">calculator2.php - 3.6</a><br />
+            <a href="http://jesseevers.com/chapter_3/index2.php">index2.php - 3.7</a><br />
+            <a href="http://jesseevers.com/chapter_3/calculator3.php">calculator3.php - 3.8</a><br />
+            <a href="http://jesseevers.com/chapter_3/calculator4.php">calculator4.php - 3.9</a><br />
+            <a href="http://jesseevers.com/chapter_3/calculator5.php">calculator5.php - 3.10</a><br />
 
             <h4>Chapter 8</h4>
 
-            <a href="http://jesseevers.zxq.net/chapter_8/display_errors.php">display_errors.php - 8.1</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_8/report_errors.php">report_errors.php - 8.2</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_8/handle_errors.php">handle_errors.php - 8.3</a><br />
+            <a href="http://jesseevers.com/chapter_8/display_errors.php">display_errors.php - 8.1</a><br />
+            <a href="http://jesseevers.com/chapter_8/report_errors.php">report_errors.php - 8.2</a><br />
+            <a href="http://jesseevers.com/chapter_8/handle_errors.php">handle_errors.php - 8.3</a><br />
 
             <h4>Chapter 9</h4>
 
-            <a href="http://jesseevers.zxq.net/chapter_9/index.php">index.php - 9.1</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_9/register.php">register.php - 9.5</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_9/view_users.php">view_users.php - 9.6</a><br />
-            <a href="http://jesseevers.zxq.net/chapter_9/password.php">password.php - 9.7</a><br />
+            <a href="http://jesseevers.com/chapter_9/index.php">index.php - 9.1</a><br />
+            <a href="http://jesseevers.com/chapter_9/register.php">register.php - 9.5</a><br />
+            <a href="http://jesseevers.com/chapter_9/view_users.php">view_users.php - 9.6</a><br />
+            <a href="http://jesseevers.com/chapter_9/password.php">password.php - 9.7</a><br />
 
             <h4>Chapter 10</h4>
 
+            <a href="http://jesseevers.com/chapter_10/delete_user.php">delete_user.php - 10.2</a><br />
+            <a href="http://jesseevers.com/chapter_10/edit_user.php">edit_user.php - 10.3</a><br />
+            <a href="http://jesseevers.com/chapter_10/view_users.php">view_users.php - 10.4</a><br />
 
             <h4>Chapter 11</h4>
 
+            <a href="http://jesseevers.com/chapter_11/email.php">email.php - 11.1</a><br />
+            <a href="http://jesseevers.com/chapter_11/display_errors.php">upload_image.php - 11.2</a><br />
+            <a href="http://jesseevers.com/chapter_11/display_errors.php">images.php - 11.6</a><br />
 
             <h4>Chapter 12</h4>
 
+            <a href="http://jesseevers.com/chapter_12/logout.php">logout.php - 12.11</a><br />
+            <a href="http://jesseevers.com/chapter_12/login.php">login.php - 12.12</a><br />
+            <a href="http://jesseevers.com/chapter_12/loggedin.php">loggedin.php - 12.13</a><br />
 
             <h4>Chapter 13</h4>
 
-
+            <a href="http://jesseevers.com/chapter_13/email.php">email.php - 13.1</a><br />
+            <a href="http://jesseevers.com/chapter_13/calculator.php">calculator.php - 13.2</a><br />
+            <a href="http://jesseevers.com/chapter_13/upload_rtf.php">upload_rtf.php - 13.3</a><br />
+            <a href="http://jesseevers.com/chapter_13/xss.php">xss.php - 13.4</a><br />
+            <a href="http://jesseevers.com/chapter_13/post_message.php">post_message.php - 13.6</a><br />
+            
             <h4>Chapter 14</h4>
 
+            <a href="http://jesseevers.com/chapter_14/pcre.php">pcre.php - 14.1</a><br />
+            <a href="http://jesseevers.com/chapter_14/matches.php">matches.php - 14.1</a><br />
+            <a href="http://jesseevers.com/chapter_14/replace.php">replace.php - 14.1</a><br />
 
             <h4>Chapter 15</h4>
 
