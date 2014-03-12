8217ac3b3f24101edffc3f051ab6d93b2162dbab
LittleGoose <jesse27999@gmail.com>
2014-03-12 14:43:32 -0400

    Added index.php

---
+++ chapter_17/index.php
@@ -0,0 +1,16 @@
+<?php 
+# Script 17.3
+# Made by Jesse Evers
+# Made on 3/12/14
+
+# This is the main page for the site
+
+# Include the HTML header
+include('includes/header.html');
+
+# The content on this page is introductory text pulled from the database, based upon the selected language
+echo $words['intro'];
+
+# Include the HTML footer file
+include ('includes/footer.hmtl');
+?>
