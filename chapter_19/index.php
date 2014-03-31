15427c2ef677ca28d84a3509bf26df8ef96e4e76
LittleGoose <jesse27999@gmail.com>
2014-03-31 16:07:08 -0400

    Finished header.html and index.php

---
+++ chapter_19/index.php
@@ -0,0 +1,17 @@
+<?php 
+# Script 19.4
+# Made by Jesse Evers
+# Made on 3/31/14
+
+# This is the main page for the site
+
+# Set the page title and include the $page_title
+$page_title = 'Make an Impression!';
+include('includes/header.html');
+?>
+
+<p>Welcome to our site....please use the links above...blah, blah, blah.</p>
+<p>Welcome to our site....please use the links above...blah, blah, blah.</p>
+
+<?php
+include('includes/footer.html');
