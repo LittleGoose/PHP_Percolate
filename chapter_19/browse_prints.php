49f87cd9fd8497bb877859544dcd86d328055381
LittleGoose <jesse27999@gmail.com>
2014-03-31 16:21:20 -0400

    Added all the necessary images as well as footer.html and browse_prints.php

---
+++ chapter_19/browse_prints.php
@@ -0,0 +1,13 @@
+<?php
+# Script 19.6
+# Made by Jesse Evers
+# Made on 3/31/14
+
+# This page displays the available prints (products)
+
+# Set the page title and include the HTML header
+$page_title = 'Browse the Prints';
+include('includes/header.html');
+
+require('../../mysqli_connect4.php;');
+?>
